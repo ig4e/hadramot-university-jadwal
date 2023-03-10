ac5b7b03ac934d6b2b3c14fddd63818f98402c30
Sekai <ahmedx793@gmail.com>
2023-03-10 02:42:38 +0200

    moved backend to express

--- renderer/utils/trpc.ts
+++ renderer/utils/trpc.ts
@@ -1,8 +1,7 @@
 import { httpBatchLink } from "@trpc/client";
 import { createTRPCNext } from "@trpc/next";
 import superjson from "superjson";
-import { AppRouter } from "../server/routers/_app";
-// import { AppRouter } from "../../main/server/routers/_app";
+import { AppRouter } from "../../main/server/routers/_app";
 
 function getBaseUrl() {
 	if (typeof window !== "undefined")
